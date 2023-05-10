dfa5526ac118d5608252b50b996c300d04476501
lucenasoft <getugbr@gmail.com>
2023-05-10 20:35:14 -0300

    feat: initial project created

---
+++ src/main/java/com/lucenasoft/workshopmongo/Startup.java
@@ -0,0 +1,13 @@
+package com.lucenasoft.workshopmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Startup {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Startup.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucenasoft/workshopmongo/StartupTests.java
@@ -0,0 +1,13 @@
+package com.lucenasoft.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
