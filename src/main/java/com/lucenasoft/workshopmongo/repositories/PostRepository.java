d27360e5f8a2ae35b40ac61cfe178173bfe986b4
lucenasoft <getugbr@gmail.com>
2023-05-15 10:30:55 -0300

    feat: adding query "mongodb" for find by title

--- src/main/java/com/lucenasoft/workshopmongo/repositories/PostRepository.java
+++ src/main/java/com/lucenasoft/workshopmongo/repositories/PostRepository.java
@@ -2,9 +2,12 @@ package com.lucenasoft.workshopmongo.repositories;
 
 import com.lucenasoft.workshopmongo.models.PostModel;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import java.util.List;
 
 public interface PostRepository extends MongoRepository<PostModel, String> {
+    @Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+    List<PostModel> searchTitle(String text); // metodo do mongodb
     List<PostModel> findByTitleContainingIgnoreCase(String text);
 }
